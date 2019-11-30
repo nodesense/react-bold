309f15e6d4ec788ea0a342ababadafde748ce643
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2019-11-30 10:38:56 +0530

    redux

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import withStorage from './Storage';
 
+function useFriendStatus(friendID) {
+    const [isOnline, setIsOnline] = useState(null);
+  
+    function handleStatusChange(status) {
+      setIsOnline(status);
+    }
+    
+    useEffect(() => {
+      
+      let handle = setInterval(() => {
+          console.log('timer running');
+        handleStatusChange(Math.ceil(100 * Math.random()) % 2 == 0 ? true: false)
+      }, 5000);
+      return () => {
+        console.log('exit/cleanup/');
+        clearInterval(handle);
+      };
+    });
+  
+    return isOnline;
+  }
+
+// useState for setState of classes, but no merging
+// useEffect for dom like did mount, did update, will unmount
+
+
 const Home = (props) => {
     console.log('Home ', props);
+    const [count, setCount] = useState(0);
+
+    // const isOnline = useFriendStatus(1);
+    // console.log('isOnline', isOnline)
+
+     // Similar to componentDidMount and componentDidUpdate:
+//   useEffect(() => {
+//     // Update the document title using the browser API
+//     document.title = `You clicked ${count} times`;
+//   });
+
     return (
         <div>
             <h2>Home Page</h2>
 
             <button onClick={ () => props.history.push("/cart")}>Cart page</button>
+        
+            <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+{/* 
+      <li style={{ color: isOnline ? 'green' : 'black' }}>
+     Someone
+    </li> */}
+        
         </div>
     )
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 // configure store
-import {createStore, combineReducers} from 'redux';
+import {createStore,
+        combineReducers,
+        bindActionCreators,
+        applyMiddleware } from 'redux';
+
+import thunk from 'redux-thunk';
 
 //action-types.js
 export const INCREMENT = "COUNTER.INCREMENT"
@@ -39,8 +44,44 @@ export const counterReducer = (state = INITIAL_STATE, action) => {
     }
 }
 
-// expect(counterReducer(0, {type: 'INCREENT', payload: value: 2})).toBe(2)
+//middlewares.js
 
+export function loggerMiddleware(store) {
+    // called during initialization
+    return function(nextFunc) {
+        // called during initialization
+        return function(action) {
+            // called for every dispatch
+            console.log("ACTION DISPATCH START", typeof action, action);
+
+            // thunk logic
+            // if (typeof action === 'function') {
+            //     return action(store.dispatch, store.getState);  // return, avoiding next/forward to reducer
+            // }
+
+            // forward action to next middleware/reducers
+            const result = nextFunc(action); // call all reducers
+            console.log("ACTION DISPATCH DONE", action);
+            return false;
+        }
+    }
+}
+
+export const cacheMiddleware = ({getState, dispatch}) => next => action => {
+    const result = next(action);
+    
+    if (typeof action === 'object' && action.type.indexOf('COUNTER.') >= 0) {
+        const state =  getState();
+        localStorage.setItem("counter", state.counter)
+    }
+
+    return result;
+}
+
+
+
+
+ 
 console.log('creating store now');
 // createStore internally dispatch action to initialize store 
 
@@ -51,8 +92,21 @@ const rootReducer = combineReducers({
     //...
 });
 
-const store = createStore(rootReducer, {counter: 1000} );
+const store = createStore(rootReducer, 
+                          {counter: 1000} ,
+                          applyMiddleware(loggerMiddleware, cacheMiddleware, thunk),  
+                          );
 export default store;
+
+
+// ---
+
+// susbcribe.
+// called after dispatch method, called after executing all the reducers, updating state
+// returns a unique unsubscribe func, to unsubscribe()
+const unsubscribe1Func = store.subscribe(function callback() {
+    console.log('subscribe 1', store.getState())
+});
 
 let state = store.getState()
 console.log("counter is ", state.counter)
@@ -69,6 +123,60 @@ console.log('dispatch action')
 store.dispatch(action);
 console.log('state is ', store.getState())
 
+
+// unsubscribe
+unsubscribe1Func()
+
+
 action = increment(2) // using action creators
 store.dispatch(action);
 console.log('state is ', store.getState())
+
+
+// this create a new wrapper function, 
+// that calls the increment with arguments => returns action 
+// and dispatch the action
+const incrementActionDispatcherFunc = bindActionCreators(increment, store.dispatch);
+
+incrementActionDispatcherFunc(7);
+console.log('state is ', store.getState())
+
+const {dispatch, getState} = store;
+
+const resetActionDispatcher = bindActionCreators(reset, dispatch);
+resetActionDispatcher();
+console.log('state is ', store.getState())
+
+// actions is an object, contains 3 functions
+const actions = {
+    increment: increment, 
+    decrement, 
+    reset
+}
+
+// bind all actions inside objects
+// returns an object contains 3 binded functions {increment, decrement, reset binded already}
+const actionsDispatcher = bindActionCreators(actions, dispatch);
+
+actionsDispatcher.decrement(2);
+console.log('state is ', store.getState())
+
+// an action creator returns a function as action
+function delayedIncrementActionCreator(value) {
+    console.log('start delayedIncrementActionCreator')
+    
+    // called by middleware, store is passed from middleware
+    return function(dispatch, getState) {
+        console.log('async function is called');
+        setTimeout( () => {
+            console.log('inside timeout delayedIncrementActionCreator')
+            const action = increment(value);
+            dispatch(action);
+            console.log('state is ', getState());
+         }, 5000);
+    }
+     console.log('end delayedIncrementActionCreator')
+}
+
+const actionFunc = delayedIncrementActionCreator(10);
+dispatch(actionFunc); // dispatching function as action
